1e1e7136322d8f26850e1d7ebd5fa460c5dfd8e3
Mohitahirwar7440 <167208181+Mohitahirwar7440@users.noreply.github.com>
2025-04-26 11:32:26 +0530

    Update Decentralize Exam System.sol

--- contract/Decentralize Exam System.sol
+++ contract/Decentralize Exam System.sol
@@ -11,11 +11,15 @@ contract DecentralizedExamSystem {
     }
 
     mapping(uint256 => Exam) public exams;
+    mapping(uint256 => mapping(address => uint8)) public results;
+
+    mapping(address => bool) public registeredStudents;
 
     event ExamScheduled(uint256 examId, string subject, uint256 date);
     event ExamResultPublished(uint256 examId, address student, uint8 score);
-
-    mapping(uint256 => mapping(address => uint8)) public results;
+    event StudentRegistered(address student);
+    event ExamRemoved(uint256 examId);
+    event AdminChanged(address oldAdmin, address newAdmin);
 
     constructor() {
         admin = msg.sender;
@@ -33,8 +37,34 @@ contract DecentralizedExamSystem {
 
     function publishResult(uint256 examId, address student, uint8 score) public onlyAdmin {
         require(exams[examId].isScheduled, "Exam not scheduled");
+        require(registeredStudents[student], "Student not registered");
         results[examId][student] = score;
         emit ExamResultPublished(examId, student, score);
     }
+
+    function registerStudent(address student) public onlyAdmin {
+        registeredStudents[student] = true;
+        emit StudentRegistered(student);
+    }
+
+    function isStudentRegistered(address student) public view returns (bool) {
+        return registeredStudents[student];
+    }
+
+    function getResult(uint256 examId, address student) public view returns (uint8) {
+        require(exams[examId].isScheduled, "Exam not scheduled");
+        return results[examId][student];
+    }
+
+    function removeExam(uint256 examId) public onlyAdmin {
+        require(exams[examId].isScheduled, "Exam not found");
+        delete exams[examId];
+        emit ExamRemoved(examId);
+    }
+
+    function changeAdmin(address newAdmin) public onlyAdmin {
+        require(newAdmin != address(0), "Invalid address");
+        emit AdminChanged(admin, newAdmin);
+        admin = newAdmin;
+    }
 }
-v
