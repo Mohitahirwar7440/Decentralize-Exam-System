60c66c0008a42bd2a45575bd1c5f182c4ab66d9f
Mohitahirwar7440 <167208181+Mohitahirwar7440@users.noreply.github.com>
2025-04-29 21:02:30 +0530

    Update Decentralize Exam System.sol

--- contract/Decentralize Exam System.sol
+++ contract/Decentralize Exam System.sol
@@ -12,8 +12,8 @@ contract DecentralizedExamSystem {
 
     mapping(uint256 => Exam) public exams;
     mapping(uint256 => mapping(address => uint8)) public results;
-
     mapping(address => bool) public registeredStudents;
+    address[] private studentList;
 
     event ExamScheduled(uint256 examId, string subject, uint256 date);
     event ExamResultPublished(uint256 examId, address student, uint8 score);
@@ -48,59 +48,8 @@ contract DecentralizedExamSystem {
     function registerStudent(address student) public onlyAdmin {
         require(!registeredStudents[student], "Student already registered");
         registeredStudents[student] = true;
+        studentList.push(student);
         emit StudentRegistered(student);
     }
 
-    function deregisterStudent(address student) public onlyAdmin {
-        require(registeredStudents[student], "Student not registered");
-        registeredStudents[student] = false;
-        emit StudentDeregistered(student);
-    }
-
-    function isStudentRegistered(address student) public view returns (bool) {
-        return registeredStudents[student];
-    }
-
-    function getResult(uint256 examId, address student) public view returns (uint8) {
-        require(exams[examId].isScheduled, "Exam not scheduled");
-        require(registeredStudents[student], "Student not registered");
-        return results[examId][student];
-    }
-
-    function removeExam(uint256 examId) public onlyAdmin {
-        require(exams[examId].isScheduled, "Exam not found");
-        delete exams[examId];
-        emit ExamRemoved(examId);
-    }
-
-    function changeAdmin(address newAdmin) public onlyAdmin {
-        require(newAdmin != address(0), "Invalid address");
-        emit AdminChanged(admin, newAdmin);
-        admin = newAdmin;
-    }
-
-    // --------- New Functions Below ---------
-
-    // View exam details by ID
-    function getExamDetails(uint256 examId) public view returns (string memory subject, uint256 date, bool isScheduled) {
-        Exam memory exam = exams[examId];
-        require(exam.isScheduled, "Exam not scheduled");
-        return (exam.subject, exam.date, exam.isScheduled);
-    }
-
-    // List multiple exam details (batch retrieval)
-    function getMultipleExams(uint256[] memory examIds) public view returns (Exam[] memory) {
-        Exam[] memory examList = new Exam[](examIds.length);
-        for (uint256 i = 0; i < examIds.length; i++) {
-            examList[i] = exams[examIds[i]];
-        }
-        return examList;
-    }
-
-    // Allow students to view their own result
-    function viewMyResult(uint256 examId) public view returns (uint8) {
-        require(registeredStudents[msg.sender], "You are not a registered student");
-        require(exams[examId].isScheduled, "Exam not scheduled");
-        return results[examId][msg.sender];
-    }
-}
+    function deregisterStudent(address
